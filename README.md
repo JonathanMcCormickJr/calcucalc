c6fbde59fc443bc958c52aa56d8df2072a43c0c3
Jonathan A. McCormick, Jr. <67705789+JonathanMcCormickJr@users.noreply.github.com>
2025-02-11 21:46:03 -0600

    Create Monomial struct

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 A Rust library for doing calculus. 
 
+This is very much a work in progress at this stage. Feel free to contribute any suggestions via a pull request. 
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::io::Error;
+
+#[derive(Debug, PartialEq)]
+struct Monomial {
+    coefficient: u8,
+    power: u8,
 }
+
+impl Monomial {
+    fn add_monomial_of_same_power(&self, other: Monomial) -> Result<Monomial, Error> {
+        if self.power == other.power {
+            Ok(Monomial{coefficient: self.coefficient + other.coefficient, power: self.power})
+        } else {
+            return Err(Error::new(std::io::ErrorKind::InvalidInput, "cannot combine monomials of different powers of x"));
+        }
+    }
+
+}
+
+// RESUME HERE...
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_monomial() {
+        assert_eq!(Monomial{coefficient: 1, power: 1}, Monomial{coefficient: 1, power: 1});
+        assert_ne!(Monomial{coefficient: 1, power: 1}, Monomial{coefficient: 0, power: 1});
+        assert_ne!(Monomial{coefficient: 1, power: 1}, Monomial{coefficient: 1, power: 0});
+
+        // ...AND HERE
+        
     }
 }
