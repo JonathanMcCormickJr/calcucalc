8e506080dbf190f25087d2941da6a8ce646a4769
Jonathan A. McCormick, Jr. <67705789+JonathanMcCormickJr@users.noreply.github.com>
2025-02-15 12:56:36 -0600

    Migrating coefficient and exponent to f64.

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@
 /// A monomial is a polynomial with only one term. It is a product of a coefficient and a power of x.
 #[derive(Clone, Debug, PartialEq)]
 struct Monomial {
-    c: i16, // Coefficient
-    e: i16, // Exponent
+    c: f64, // Coefficient
+    e: f64, // Exponent
 }
 
 impl Monomial {
@@ -78,7 +78,7 @@ impl Polynomial {
     /// Sort the elements of the polynomial by the exponent of x.
     fn sort_by_exponent(&self) -> Polynomial {
         let mut elements = self.0.clone();
-        elements.sort_by(|a, b| b.e.cmp(&a.e));
+        elements.sort_by(|a, b| b.e.partial_cmp(&a.e).unwrap());
         Polynomial(elements)
     }
 
@@ -114,96 +114,153 @@ mod tests {
 
     #[test]
     fn test_monomial_identity() {
-        assert_eq!(Monomial { c: 1, e: 1 }, Monomial { c: 1, e: 1 });
-        assert_ne!(Monomial { c: 1, e: 1 }, Monomial { c: 0, e: 1 });
-        assert_ne!(Monomial { c: 1, e: 1 }, Monomial { c: 1, e: 0 });
+        assert_eq!(
+            Monomial { c: 1_f64, e: 1_f64 },
+            Monomial { c: 1_f64, e: 1_f64 }
+        );
+        assert_ne!(
+            Monomial { c: 1_f64, e: 1_f64 },
+            Monomial { c: 0_f64, e: 1_f64 }
+        );
+        assert_ne!(
+            Monomial { c: 1_f64, e: 1_f64 },
+            Monomial { c: 1_f64, e: 0_f64 }
+        );
     }
 
     #[test]
     fn test_add_monomial_of_same_power() {
-        let m1 = Monomial { c: 1, e: 1 };
-        let m2 = Monomial { c: 2, e: 1 };
-        let m3 = Monomial { c: 3, e: 1 };
+        let m1 = Monomial { c: 1_f64, e: 1_f64 };
+        let m2 = Monomial { c: 2_f64, e: 1_f64 };
+        let m3 = Monomial { c: 3_f64, e: 1_f64 };
         assert_eq!(m3, m1.add_monomial_of_same_power(m2));
 
-        let m4 = Monomial { c: 45, e: 250 };
-        let m5 = Monomial { c: 57, e: 250 };
-        let m6 = Monomial { c: 102, e: 250 };
+        let m4 = Monomial {
+            c: 45_f64,
+            e: 250_f64,
+        };
+        let m5 = Monomial {
+            c: 57_f64,
+            e: 250_f64,
+        };
+        let m6 = Monomial {
+            c: 102_f64,
+            e: 250_f64,
+        };
         assert_eq!(m6, m4.add_monomial_of_same_power(m5));
 
-        let m7 = Monomial { c: 1, e: -11 };
-        let m8 = Monomial { c: 2, e: -11 };
-        let m9 = Monomial { c: 3, e: -11 };
+        let m7 = Monomial {
+            c: 1_f64,
+            e: -11_f64,
+        };
+        let m8 = Monomial {
+            c: 2_f64,
+            e: -11_f64,
+        };
+        let m9 = Monomial {
+            c: 3_f64,
+            e: -11_f64,
+        };
         assert_eq!(m9, m7.add_monomial_of_same_power(m8));
 
-        let m10 = Monomial { c: 1, e: 0 };
-        let m11 = Monomial { c: 2, e: 0 };
-        let m12 = Monomial { c: 3, e: 0 };
+        let m10 = Monomial { c: 1_f64, e: 0_f64 };
+        let m11 = Monomial { c: 2_f64, e: 0_f64 };
+        let m12 = Monomial { c: 3_f64, e: 0_f64 };
         assert_eq!(m12, m10.add_monomial_of_same_power(m11));
     }
 
     #[test]
     #[should_panic(expected = "Cannot add monomials with different powers of x.")]
     fn test_panic_on_adding_monomial_of_different_power0() {
-        let m1 = Monomial { c: 1, e: 1 };
-        let m2 = Monomial { c: 2, e: 2 };
+        let m1 = Monomial { c: 1_f64, e: 1_f64 };
+        let m2 = Monomial { c: 2_f64, e: 2_f64 };
         m1.add_monomial_of_same_power(m2);
     }
 
     #[test]
     #[should_panic(expected = "Cannot add monomials with different powers of x.")]
     fn test_panic_on_adding_monomial_of_different_power1() {
-        let m3 = Monomial { c: 1, e: 0 };
-        let m4 = Monomial { c: 2, e: 1 };
+        let m3 = Monomial { c: 1_f64, e: 0_f64 };
+        let m4 = Monomial { c: 2_f64, e: 1_f64 };
         m3.add_monomial_of_same_power(m4);
     }
 
     #[test]
     #[should_panic(expected = "Cannot add monomials with different powers of x.")]
     fn test_panic_on_adding_monomial_of_different_power2() {
-        let m5 = Monomial { c: 1, e: -1 };
-        let m6 = Monomial { c: 2, e: 0 };
+        let m5 = Monomial {
+            c: 1_f64,
+            e: -1_f64,
+        };
+        let m6 = Monomial { c: 2_f64, e: 0_f64 };
         m5.add_monomial_of_same_power(m6);
     }
 
     #[test]
     #[should_panic(expected = "Cannot add monomials with different powers of x.")]
     fn test_panic_on_adding_monomial_of_different_power3() {
-        let m7 = Monomial { c: 1, e: -1 };
-        let m8 = Monomial { c: 2, e: 1 };
+        let m7 = Monomial {
+            c: 1_f64,
+            e: -1_f64,
+        };
+        let m8 = Monomial { c: 2_f64, e: 1_f64 };
         m7.add_monomial_of_same_power(m8);
     }
 
     #[test]
     fn test_multiply_monomial() {
-        let m1 = Monomial { c: 2, e: 1 };
-        let m2 = Monomial { c: 3, e: 2 };
-        let m3 = Monomial { c: 6, e: 3 };
+        let m1 = Monomial { c: 2_f64, e: 1_f64 };
+        let m2 = Monomial { c: 3_f64, e: 2_f64 };
+        let m3 = Monomial { c: 6_f64, e: 3_f64 };
         assert_eq!(m3, m1.multiply_monomial(m2));
 
-        let m4 = Monomial { c: 45, e: 250 };
-        let m5 = Monomial { c: 57, e: 250 };
-        let m6 = Monomial { c: 2565, e: 500 };
+        let m4 = Monomial {
+            c: 45_f64,
+            e: 250_f64,
+        };
+        let m5 = Monomial {
+            c: 57_f64,
+            e: 250_f64,
+        };
+        let m6 = Monomial {
+            c: 2565_f64,
+            e: 500_f64,
+        };
         assert_eq!(m6, m4.multiply_monomial(m5));
 
-        let m7 = Monomial { c: 1, e: -11 };
-        let m8 = Monomial { c: 2, e: -11 };
-        let m9 = Monomial { c: 2, e: -22 };
+        let m7 = Monomial {
+            c: 1_f64,
+            e: -11_f64,
+        };
+        let m8 = Monomial {
+            c: 2_f64,
+            e: -11_f64,
+        };
+        let m9 = Monomial {
+            c: 2_f64,
+            e: -22_f64,
+        };
         assert_eq!(m9, m7.multiply_monomial(m8));
 
-        let m10 = Monomial { c: 1, e: 0 };
-        let m11 = Monomial { c: 2, e: 0 };
-        let m12 = Monomial { c: 2, e: 0 };
+        let m10 = Monomial { c: 1_f64, e: 0_f64 };
+        let m11 = Monomial { c: 2_f64, e: 0_f64 };
+        let m12 = Monomial { c: 2_f64, e: 0_f64 };
         assert_eq!(m12, m10.multiply_monomial(m11));
 
-        let m13 = Monomial { c: 1, e: 0 };
-        let m14 = Monomial { c: 0, e: 500 };
-        let m15 = Monomial { c: 0, e: 500 };
+        let m13 = Monomial { c: 1_f64, e: 0_f64 };
+        let m14 = Monomial {
+            c: 0_f64,
+            e: 500_f64,
+        };
+        let m15 = Monomial {
+            c: 0_f64,
+            e: 500_f64,
+        };
         assert_eq!(m15, m13.multiply_monomial(m14));
 
-        let m16 = Monomial { c: 0, e: 0 };
-        let m17 = Monomial { c: 0, e: 0 };
-        let m18 = Monomial { c: 0, e: 0 };
+        let m16 = Monomial { c: 0_f64, e: 0_f64 };
+        let m17 = Monomial { c: 0_f64, e: 0_f64 };
+        let m18 = Monomial { c: 0_f64, e: 0_f64 };
         assert_eq!(m18, m16.multiply_monomial(m17));
     }
 
@@ -211,24 +268,27 @@ mod tests {
 
     #[test]
     fn test_polynomial_identity() {
-        let p1 = Polynomial(vec![Monomial { c: 1, e: 1 }]);
-        let p2 = Polynomial(vec![Monomial { c: 1, e: 1 }]);
+        let p1 = Polynomial(vec![Monomial { c: 1_f64, e: 1_f64 }]);
+        let p2 = Polynomial(vec![Monomial { c: 1_f64, e: 1_f64 }]);
         assert_eq!(p1, p2);
 
-        let p3 = Polynomial(vec![Monomial { c: 1, e: 1 }]);
-        let p4 = Polynomial(vec![Monomial { c: 1, e: 0 }]);
+        let p3 = Polynomial(vec![Monomial { c: 1_f64, e: 1_f64 }]);
+        let p4 = Polynomial(vec![Monomial { c: 1_f64, e: 0_f64 }]);
         assert_ne!(p3, p4);
 
-        let p5 = Polynomial(vec![Monomial { c: 1, e: 1 }]);
-        let p6 = Polynomial(vec![Monomial { c: 0, e: 1 }]);
+        let p5 = Polynomial(vec![Monomial { c: 1_f64, e: 1_f64 }]);
+        let p6 = Polynomial(vec![Monomial { c: 0_f64, e: 1_f64 }]);
         assert_ne!(p5, p6);
 
-        let p7 = Polynomial(vec![Monomial { c: 1, e: 1 }]);
-        let p8 = Polynomial(vec![Monomial { c: 0, e: 0 }]);
+        let p7 = Polynomial(vec![Monomial { c: 1_f64, e: 1_f64 }]);
+        let p8 = Polynomial(vec![Monomial { c: 0_f64, e: 0_f64 }]);
         assert_ne!(p7, p8);
 
-        let p9 = Polynomial(vec![Monomial { c: 1, e: 1 }]);
-        let p10 = Polynomial(vec![Monomial { c: 1, e: 1 }, Monomial { c: 1, e: 1 }]);
+        let p9 = Polynomial(vec![Monomial { c: 1_f64, e: 1_f64 }]);
+        let p10 = Polynomial(vec![
+            Monomial { c: 1_f64, e: 1_f64 },
+            Monomial { c: 1_f64, e: 1_f64 },
+        ]);
         assert_ne!(p9, p10);
     }
 
@@ -248,100 +308,189 @@ mod tests {
 
     #[test]
     fn test_simplify_by_combining_alike_powers() {
-        let p1 = Polynomial(vec![Monomial { c: 1, e: 1 }, Monomial { c: 2, e: 1 }]);
-        let p2 = Polynomial(vec![Monomial { c: 3, e: 1 }]);
+        let p1 = Polynomial(vec![
+            Monomial { c: 1_f64, e: 1_f64 },
+            Monomial { c: 2_f64, e: 1_f64 },
+        ]);
+        let p2 = Polynomial(vec![Monomial { c: 3_f64, e: 1_f64 }]);
         assert_eq!(p2, p1.simplified());
 
         let p3 = Polynomial(vec![
-            Monomial { c: 1, e: 1 },
-            Monomial { c: 2, e: 1 },
-            Monomial { c: 3, e: 1 },
+            Monomial { c: 1_f64, e: 1_f64 },
+            Monomial { c: 2_f64, e: 1_f64 },
+            Monomial { c: 3_f64, e: 1_f64 },
         ]);
-        let p4 = Polynomial(vec![Monomial { c: 6, e: 1 }]);
+        let p4 = Polynomial(vec![Monomial { c: 6_f64, e: 1_f64 }]);
         assert_eq!(p4, p3.simplified());
 
         let p5 = Polynomial(vec![
-            Monomial { c: -1, e: 1 },
-            Monomial { c: -2, e: 1 },
-            Monomial { c: -3, e: 1 },
-            Monomial { c: 4, e: 1 },
+            Monomial {
+                c: -1_f64,
+                e: 1_f64,
+            },
+            Monomial {
+                c: -2_f64,
+                e: 1_f64,
+            },
+            Monomial {
+                c: -3_f64,
+                e: 1_f64,
+            },
+            Monomial { c: 4_f64, e: 1_f64 },
         ]);
-        let p6 = Polynomial(vec![Monomial { c: -2, e: 1 }]);
+        let p6 = Polynomial(vec![Monomial {
+            c: -2_f64,
+            e: 1_f64,
+        }]);
         assert_eq!(p6, p5.simplified());
 
         let p7 = Polynomial(vec![
-            Monomial { c: 1, e: 1 },
-            Monomial { c: 2, e: 1 },
-            Monomial { c: 3, e: 1 },
-            Monomial { c: 4, e: 1 },
-            Monomial { c: 5, e: 1 },
+            Monomial { c: 1_f64, e: 1_f64 },
+            Monomial { c: 2_f64, e: 1_f64 },
+            Monomial { c: 3_f64, e: 1_f64 },
+            Monomial { c: 4_f64, e: 1_f64 },
+            Monomial { c: 5_f64, e: 1_f64 },
         ]);
-        let p8 = Polynomial(vec![Monomial { c: 15, e: 1 }]);
+        let p8 = Polynomial(vec![Monomial {
+            c: 15_f64,
+            e: 1_f64,
+        }]);
         assert_eq!(p8, p7.simplified());
 
         let p9 = Polynomial(vec![
-            Monomial { c: 1, e: 1 },
-            Monomial { c: 2, e: 1 },
-            Monomial { c: 3, e: 1 },
-            Monomial { c: 4, e: 1 },
-            Monomial { c: 5, e: 1 },
-            Monomial { c: 6, e: 1 },
+            Monomial { c: 1_f64, e: 1_f64 },
+            Monomial { c: 2_f64, e: 1_f64 },
+            Monomial { c: 3_f64, e: 1_f64 },
+            Monomial { c: 4_f64, e: 1_f64 },
+            Monomial { c: 5_f64, e: 1_f64 },
+            Monomial { c: 6_f64, e: 1_f64 },
         ]);
-        let p10 = Polynomial(vec![Monomial { c: 21, e: 1 }]);
+        let p10 = Polynomial(vec![Monomial {
+            c: 21_f64,
+            e: 1_f64,
+        }]);
         assert_eq!(p10, p9.simplified());
 
         let p11 = Polynomial(vec![
-            Monomial { c: 1, e: 1 },
-            Monomial { c: 2, e: 1 },
-            Monomial { c: 3, e: 1 },
-            Monomial { c: 4, e: 1 },
-            Monomial { c: 5, e: 1 },
-            Monomial { c: 6, e: 1 },
-            Monomial { c: 7, e: 1 },
+            Monomial { c: 1_f64, e: 1_f64 },
+            Monomial { c: 2_f64, e: 1_f64 },
+            Monomial { c: 3_f64, e: 1_f64 },
+            Monomial { c: 4_f64, e: 1_f64 },
+            Monomial { c: 5_f64, e: 1_f64 },
+            Monomial { c: 6_f64, e: 1_f64 },
+            Monomial { c: 7_f64, e: 1_f64 },
         ]);
-        let p12 = Polynomial(vec![Monomial { c: 28, e: 1 }]);
+        let p12 = Polynomial(vec![Monomial {
+            c: 28_f64,
+            e: 1_f64,
+        }]);
         assert_eq!(p12, p11.simplified());
 
         let p13 = Polynomial(vec![
-            Monomial { c: 1, e: -16 },
-            Monomial { c: -2, e: -1 },
-            Monomial { c: 3, e: 1 },
-            Monomial { c: -344, e: -50 },
-            Monomial { c: 5, e: 30 },
-            Monomial { c: -5, e: 30 },
-            Monomial { c: 7, e: 1 },
-            Monomial { c: 8, e: 1 },
+            Monomial {
+                c: 1_f64,
+                e: -16_f64,
+            },
+            Monomial {
+                c: -2_f64,
+                e: -1_f64,
+            },
+            Monomial { c: 3_f64, e: 1_f64 },
+            Monomial {
+                c: -344_f64,
+                e: -50_f64,
+            },
+            Monomial {
+                c: 5_f64,
+                e: 30_f64,
+            },
+            Monomial {
+                c: -5_f64,
+                e: 30_f64,
+            },
+            Monomial { c: 7_f64, e: 1_f64 },
+            Monomial { c: 8_f64, e: 1_f64 },
         ]);
         let p14 = Polynomial(vec![
-            Monomial { c: 0, e: 30 },
-            Monomial { c: 18, e: 1 },
-            Monomial { c: -2, e: -1 },
-            Monomial { c: 1, e: -16 },
-            Monomial { c: -344, e: -50 },
+            Monomial {
+                c: 0_f64,
+                e: 30_f64,
+            },
+            Monomial {
+                c: 18_f64,
+                e: 1_f64,
+            },
+            Monomial {
+                c: -2_f64,
+                e: -1_f64,
+            },
+            Monomial {
+                c: 1_f64,
+                e: -16_f64,
+            },
+            Monomial {
+                c: -344_f64,
+                e: -50_f64,
+            },
         ]);
         assert_eq!(p14.simplified(), p13.simplified());
 
         let p15 = Polynomial(vec![
-            Monomial { c: 1, e: -10 },
-            Monomial { c: 2, e: 31 },
-            Monomial { c: 3, e: 14 },
-            Monomial { c: 4, e: 94 },
-            Monomial { c: 5, e: 15 },
-            Monomial { c: 6, e: 0 },
-            Monomial { c: 7, e: 0 },
-            Monomial { c: 8, e: 31 },
-            Monomial { c: 9, e: 0 },
+            Monomial {
+                c: 1_f64,
+                e: -10_f64,
+            },
+            Monomial {
+                c: 2_f64,
+                e: 31_f64,
+            },
+            Monomial {
+                c: 3_f64,
+                e: 14_f64,
+            },
+            Monomial {
+                c: 4_f64,
+                e: 94_f64,
+            },
+            Monomial {
+                c: 5_f64,
+                e: 15_f64,
+            },
+            Monomial { c: 6_f64, e: 0_f64 },
+            Monomial { c: 7_f64, e: 0_f64 },
+            Monomial {
+                c: 8_f64,
+                e: 31_f64,
+            },
+            Monomial { c: 9_f64, e: 0_f64 },
         ]);
         let p16 = Polynomial(vec![
-            Monomial { c: 4, e: 94 },
-            Monomial { c: 10, e: 31 },
-            Monomial { c: 5, e: 15 },
-            Monomial { c: 3, e: 14 },
-            Monomial { c: 22, e: 0 },
-            Monomial { c: 1, e: -10 },
+            Monomial {
+                c: 4_f64,
+                e: 94_f64,
+            },
+            Monomial {
+                c: 10_f64,
+                e: 31_f64,
+            },
+            Monomial {
+                c: 5_f64,
+                e: 15_f64,
+            },
+            Monomial {
+                c: 3_f64,
+                e: 14_f64,
+            },
+            Monomial {
+                c: 22_f64,
+                e: 0_f64,
+            },
+            Monomial {
+                c: 1_f64,
+                e: -10_f64,
+            },
         ]);
         assert_eq!(p15.simplified(), p16.simplify_by_combining_alike_powers());
-
 
         // ADD MORE CASES HERE...
     }
